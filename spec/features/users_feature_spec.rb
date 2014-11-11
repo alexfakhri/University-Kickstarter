f9ac5a48dbdcdb79c7f35728d11d2342d40c474f
RachelNolan <mail@rachelnolan.com>
2014-11-11 16:54:38 +0000

    User only sees sign out link when they are signed in

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -7,4 +7,33 @@ context "User not signed in and on the home page" do
 		expect(page).to have_link('User sign in')
 		expect(page).to have_link('User sign up')
 	end
+
+	it "Should not see a sign out link" do 
+		visit '/projects'
+		expect(page).not_to have_content('User sign out')
+	end
+
+end
+
+context "User signed in on the homepage" do 
+
+	before do 
+		visit '/projects'
+		click_link 'User sign up'
+		fill_in('Email', with: 'test@test.com')
+		fill_in('Password', with: '12345678')
+		fill_in('Password confirmation', with: '12345678')
+		click_button('Sign up')
+	end
+
+	it "should see a sign out link" do 
+		visit '/projects'
+		expect(page).to have_link('Sign out')
+	end 
+
+	it " should not see a 'Sign in' and 'Sign up' link" do 
+		visit '/projects'
+		expect(page).not_to have_link('User sign in')
+		expect(page).not_to have_link('User sign up')
+	end 
 end
