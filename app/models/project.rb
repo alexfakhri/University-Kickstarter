f6a96570ab9b176f65d88cf58321873a8d8392bb
slstevens <sean.stevens.123@gmail.com>
2014-11-11 17:37:28 +0000

    Added image upload

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,7 @@ class Project < ActiveRecord::Base
 	validates :title, length: {minimum: 1} 
 	validates :description, length: {minimum: 1}
 
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111155154) do
+ActiveRecord::Schema.define(version: 20141111173059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 20141111155154) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "university_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "projects", ["university_id"], name: "index_projects_on_university_id", using: :btree
@@ -34,6 +38,10 @@ ActiveRecord::Schema.define(version: 20141111155154) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
