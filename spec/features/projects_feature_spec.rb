b28e9d94fd108f7f6ccf1a2e0248f0fe116d7ebc
slstevens <sean.stevens.123@gmail.com>
2014-11-11 15:09:46 +0000

    Added funding target field and added to view

--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -12,12 +12,14 @@ describe 'projects' do
 
 	context 'projects have been added' do
 		before do
-			Project.create(title: 'Sports Hall', description: 'Test description')
+			Project.create(title: 'Sports Hall', description: 'Test description', target_amount: 20000)
 		end
 
 		it 'should display projects' do
 			visit '/projects'
 			expect(page).to have_content('Sports Hall')
+			expect(page).to have_content('Test description')
+			expect(page).to have_content('20000')
 			expect(page).not_to have_content('No projects yet')
 		end
 	end
@@ -29,6 +31,8 @@ describe 'creating projects' do
 		click_link 'Add a project'
 		fill_in 'Title', with: 'Sports Hall'
 		fill_in 'Description', with: 'Test description'
+		fill_in 'Funding target', with: "20000"
+		# fill_in 'End Date', with: "01/01/2015"
 		click_button 'Create Project'
 		expect(page).to have_content 'Sports Hall'
 		expect(page).to have_content 'Test description'
