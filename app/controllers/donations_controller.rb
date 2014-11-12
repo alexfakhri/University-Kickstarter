c031f667b8aa22abf2876d20aea31d0265fac6ab
RachelNolan <mail@rachelnolan.com>
2014-11-12 10:44:14 +0000

    User can donate to a project.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,22 @@
 class DonationsController < ApplicationController
+
+	def index
+	end
+
+	def new
+		@project = Project.find(params[:project_id])
+		@donation = Donation.new
+	end
+
+	def create 
+		@project = Project.find(params[:project_id])
+		@donation = @project.donations.new(donation_params)
+		@donation.user = current_user
+		redirect_to '/'
+	end
+
+	def donation_params
+		params.require(:donation).permit(:ammount)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   end
 
   resources :projects, only: [:index]
+    
 
   # Example resource route with options:
   #   resources :products do
