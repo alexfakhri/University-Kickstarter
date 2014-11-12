c41535306183c81998127142ef9680eb7ef07916
RachelNolan <mail@rachelnolan.com>
2014-11-12 14:07:30 +0000

    Calcuate number of backers / unique backers

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,4 +8,17 @@ class Project < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 	has_many :donations
+
+	def total_donations 
+		return 0 if donations.none?
+		donations.inject(0) {|memo, donation| memo + donation.amount} 
+	end
+
+	def unique_donations
+		donations_user_ids = []
+		self.donations.each do |donation|
+			donations_user_ids << donation.user_id
+		end	
+		return donations_user_ids.uniq.count
+	end
 end
