a7bbf539de95b4bceb358e5e24605d526751cbed
slstevens <sean.stevens.123@gmail.com>
2014-11-12 10:19:36 +0000

    University user with devise, fixed project paths following shallow routing

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+	private
+
+	def sign_up_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :university_user, :university_id)
+	end
+
+	def account_update_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :university_user, :university_id)
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :donations
+
+  belongs_to :university #only for university users
 end
