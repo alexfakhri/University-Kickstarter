238ebaf626cc69002887cde43ee8aa4c71e11930
RachelNolan <mail@rachelnolan.com>
2014-11-14 17:09:16 +0000

    Fixed donate link from individual project pages and tidied Donations code

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -14,8 +14,6 @@ before_action :authenticate_user!, :except => [:index]
 
   def create 
 
-   
-    # redirect_to '/projects' 
     customer = Stripe::Customer.create(
       :email => current_user.email,
       :card  => params[:donation][:token][:id]
@@ -33,7 +31,6 @@ before_action :authenticate_user!, :except => [:index]
     @donation.user = current_user
     @donation.save
     
-    # render json: {message: "Donation successful"}
     render js: "window.location.pathname='#{project_path(@project)}'"
 
     rescue Stripe::CardError => e
@@ -41,20 +38,6 @@ before_action :authenticate_user!, :except => [:index]
       p e.inspect
 
   end
-
-
-
-#     create charge
-#     if successful
-#       create donation
-#     end
-
- 
-#       else
-#         flash[:notice] = "This donation did not work"
-#         render 'new'
-#       end
-#   end
 
   def donation_params
     params.require(:donation).permit(:amount)
