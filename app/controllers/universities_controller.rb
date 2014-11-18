3b4a78162b2d3574cc6c310dfdc84e7c523d9e8e
danjocutler <danjocutler@gmail.com>
2014-11-18 17:16:40 +0000

    university links work

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -11,7 +11,7 @@ class UniversitiesController < ApplicationController
 
 	def show
 		@university = University.find(params[:id])
-		@project = @university.projects.find(params[:id])
+		# @project = @university.projects.find(params[:id])
 	end
 
 	def create
